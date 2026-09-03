37688341d9742ca58a856dd6f3f6bb5eead81624
blackrabbit1x0 <xuyash55@gmail.com>
2026-09-03 12:56:54 +0545

    agentgraph 0.6.0 (probabilistic paths + neo4j store)

--- Formula/agentgraph.rb
+++ Formula/agentgraph.rb
@@ -1,27 +1,27 @@
 class Agentgraph < Formula
   desc "Attack-path analysis for autonomous AI agents"
   homepage "https://github.com/blackrabbit1x0/agentgraph"
-  version "0.5.1"
+  version "0.6.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.5.1/agentgraph-darwin-amd64.tar.gz"
-      sha256 "43f411b36a593a7e07d0dbd8af7dbe9ae681d90260d786cc37841fc357f89543"
+      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.6.0/agentgraph-darwin-amd64.tar.gz"
+      sha256 "ccff09b47c96cd4d670425bd230072ac7d8658943b8a28ecd7e5a3a5872e8ff0"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.5.1/agentgraph-darwin-arm64.tar.gz"
-      sha256 "fde8b1f7da4fea7e75507797c40fd68e16ea4601e68632314aafe101f03a09cb"
+      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.6.0/agentgraph-darwin-arm64.tar.gz"
+      sha256 "1a996da4fa29e1d69cb77a4be278f0b30506ec7514515f6e1b841ce882662b60"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.5.1/agentgraph-linux-amd64.tar.gz"
-      sha256 "3016cc20608a066e460c9117a7dfa163298d394bdd27dfd0e462961e79a86891"
+      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.6.0/agentgraph-linux-amd64.tar.gz"
+      sha256 "934e43ae26c47d7967ed9f6a5ac6eede76ea923a407b3a9fa74f99f906a35bcd"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.5.1/agentgraph-linux-arm64.tar.gz"
-      sha256 "31ee358863830e456d24b43384998a436bb039e4bdb462921ae2d20835006b9f"
+      url "https://github.com/blackrabbit1x0/agentgraph/releases/download/v0.6.0/agentgraph-linux-arm64.tar.gz"
+      sha256 "aa09de15a74509eaf7c36aa2ce959523f90034b766fca1284392511afc98ccb4"
     end
   end
 
